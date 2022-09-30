776d38f02eb4d9311599ee4ac3ecc88da0035214
reita009 <julio.python777@gmail.com>
2022-09-30 15:30:48 -0300

    criando variavel de ambiente

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import  express  from "express";
 import dotenv from "dotenv";
 import mustacheExpress from "mustache-express";
 import path from "path";
-
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -17,5 +17,10 @@ server.use(express.static(path.join(__dirname, '../public')));
 
 //Rotas
 
+server.use(mainRoutes);
+
+server.use((req, res)=>{
+    res.send('Página não encontrada');
+})
 
 server.listen(process.env.PORT);
